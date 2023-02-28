05fe35a257aeff60f04aa77de4f7096ae3846c1d
Martin Ansong <martin.ansong@harness.io>
2023-02-28 14:17:54 +0000

    Add sshkey option

--- clone.sh
+++ clone.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 set -xe
 
-# If path setting is not set, then use current directory
-path=${PLUGIN_PATH:-.}
-mkdir -p ${path}
-cd ${path}
+# # If path setting is not set, then use current directory
+# path=${PLUGIN_PATH:-.}
+# mkdir -p ${path}
+# cd ${path}
 
-# Clones the public git repo and checkout to a branch
-git ${PLUGIN_ARGS} clone https://${PLUGIN_GITHUB_USER}:${PLUGIN_GITHUB_TOKEN}@github.com/${PLUGIN_REPO_URL} .
-git checkout ${PLUGIN_BRANCH}
+# # Clones the public git repo and checkout to a branch
+# git ${PLUGIN_ARGS} clone https://${PLUGIN_GITHUB_USER}:${PLUGIN_GITHUB_TOKEN}@github.com/${PLUGIN_REPO_URL} .
+# git checkout ${PLUGIN_BRANCH}
 
+if [ -n "${PLUGIN_SSH_KEY}" ]; then
+    mkdir -p ~/.ssh
+    cat << EOF > id_rsa
+    ${PLUGIN_SSH_KEY}
+EOF
+    mv id_rsa ~/.ssh
+    chmod 600 ~/.ssh/id_rsa
+fi
 # Prints the last commit
 git log -1 --stat
